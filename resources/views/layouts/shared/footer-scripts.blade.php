ea9b1bebb50ad2033667d2b533dd9cb7b6c193c3
Meon Gans <meon.gans@gmail.com>
2024-02-28 18:40:00 +0200

    add site url

--- resources/views/layouts/shared/footer-scripts.blade.php
+++ resources/views/layouts/shared/footer-scripts.blade.php
@@ -9,39 +9,39 @@
         src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/mode/xml/xml.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/dompurify/2.2.7/purify.min.js"></script>
 
-<script type="text/javascript" src="http://cprpp/editor/js/froala_editor.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/align.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/char_counter.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/code_beautifier.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/code_view.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/colors.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/draggable.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/emoticons.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/entities.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/file.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/font_size.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/font_family.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/fullscreen.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/image.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/image_manager.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/line_breaker.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/inline_style.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/link.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/lists.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/paragraph_format.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/paragraph_style.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/quick_insert.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/quote.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/table.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/save.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/url.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/video.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/help.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/print.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/third_party/spell_checker.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/special_characters.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/plugins/word_paste.min.js"></script>
-<script type="text/javascript" src="http://cprpp/editor/js/languages/uk.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/froala_editor.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/align.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/char_counter.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/code_beautifier.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/code_view.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/colors.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/draggable.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/emoticons.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/entities.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/file.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/font_size.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/font_family.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/fullscreen.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/image.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/image_manager.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/line_breaker.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/inline_style.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/link.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/lists.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/paragraph_format.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/paragraph_style.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/quick_insert.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/quote.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/table.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/save.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/url.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/video.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/help.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/print.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/third_party/spell_checker.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/special_characters.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/plugins/word_paste.min.js"></script>
+<script type="text/javascript" src="{{ config('app.url') }}/editor/js/languages/uk.js"></script>
 
 <script>
     (function () {
@@ -49,7 +49,7 @@
             language: 'uk',
             key: "1C%kZV[IX)_SL}UJHAEFZMUJOYGYQE[\\ZJ]RAe(+%$==",
             attribution: false, // to hide "Powered by Froala"
-            imageUploadURL: 'http://cprpp/admin/uploadImage',
+            imageUploadURL: '{{ config('app.url') }}/admin/uploadImage',
             imageUploadMethod: 'POST',
 
         })
--- resources/views/layouts/shared/title-meta.blade.php
+++ resources/views/layouts/shared/title-meta.blade.php
@@ -8,22 +8,22 @@
 <link rel="shortcut icon" href="/images/favicon.ico">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/froala_editor.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/froala_style.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/code_view.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/draggable.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/colors.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/emoticons.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/image_manager.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/image.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/line_breaker.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/table.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/char_counter.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/video.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/fullscreen.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/file.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/quick_insert.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/help.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/third_party/spell_checker.css">
-<link rel="stylesheet" href="http://cprpp/editor/css/plugins/special_characters.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/froala_editor.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/froala_style.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/code_view.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/draggable.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/colors.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/emoticons.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/image_manager.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/image.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/line_breaker.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/table.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/char_counter.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/video.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/fullscreen.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/file.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/quick_insert.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/help.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/third_party/spell_checker.css">
+<link rel="stylesheet" href="{{ config('app.url') }}/editor/css/plugins/special_characters.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/6.65.7/codemirror.min.css">
